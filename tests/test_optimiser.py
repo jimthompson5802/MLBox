0521516f8ba4e2170821df1a05357e0328119702
Jim Thompson <jimthompson5802@aol.com>
2019-09-23 22:12:19 -0400

    refactor: pytests to separate out mlflow specific tests

--- tests/test_optimiser.py
+++ tests/test_optimiser.py
@@ -7,8 +7,6 @@
 import pytest
 import numpy as np
 import shutil
-import os.path
-import os
 
 from mlbox.optimisation.optimiser import Optimiser
 from mlbox.preprocessing.drift_thresholder import Drift_thresholder
@@ -169,47 +167,3 @@ def test_evaluate_and_optimise_classification():
     best = opt.optimise(space, dict, 1)
     assert type(best) == type(dict)
 
-def test_evaluate_and_optimise_classification_with_mlflow(setup_for_mlflow):
-    """Test evaluate_and_optimise method of Optimiser class."""
-    reader = Reader(sep=",")
-
-    dict = reader.train_test_split(Lpath=["data_for_tests/train.csv",
-                                          "data_for_tests/test.csv"],
-                                   target_name="Survived")
-    drift_thresholder = Drift_thresholder()
-    drift_thresholder = drift_thresholder.fit_transform(dict)
-
-    with pytest.warns(UserWarning) as record:
-        opt = Optimiser(scoring='accuracy', n_folds=3)
-    assert len(record) == 1
-    dict_error = dict.copy()
-    dict_error["target"] = dict_error["target"].astype(str)
-    with pytest.raises(ValueError):
-        score = opt.evaluate(None, dict_error)
-
-    with pytest.warns(UserWarning) as record:
-        opt = Optimiser(scoring='accuracy', n_folds=3)
-    assert len(record) == 1
-    score = opt.evaluate(None, dict)
-    assert 0. <= score <= 1.
-
-    space = {'ne__numerical_strategy': {"search": "choice", "space": [0]},
-             'ce__strategy': {"search": "choice",
-                              "space": ["label_encoding"]},
-             'fs__threshold': {"search": "uniform",
-                               "space": [0.01, 0.3]},
-             'est__max_depth': {"search": "choice",
-                                "space": [3, 4, 5, 6, 7]}
-
-             }
-
-    mflow_parms = {'tracking_uri' : './mlruns',
-                   'experiment_name' : 'mlfow_experiment_name'}
-
-    best = opt.optimise(space, dict, 4, mlflow_parms = mflow_parms)
-    assert os.path.exists('./mlruns')
-    assert os.path.exists('./mlruns/1')
-    assert os.path.isfile('./mlruns/1/meta.yaml')
-    assert len(os.listdir('./mlruns/1')) == 5
-
-    assert type(best) == type(dict)
