7a746611e2db82d4ef2e71ee1c9610cf0b9caaaa
Jim Thompson <jimthompson5802@aol.com>
2019-09-28 17:21:37 -0400

    fix: adjust removal for mlflow.active flag

--- mlbox/optimisation/optimiser.py
+++ mlbox/optimisation/optimiser.py
@@ -422,26 +422,25 @@ class Optimiser():
                 ))
                 print("")
 
-            if self.mlflow_active:
-                mlflow.start_run()
+            mlflow.start_run()
 
-                na_params = ne.get_params()
-                for k in na_params.keys():
-                    mlflow.log_param('na__'+k, na_params[k])
+            na_params = ne.get_params()
+            for k in na_params.keys():
+                mlflow.log_param('na__'+k, na_params[k])
 
-                mlflow.log_param('cat__encoding', ce.strategy)
+            mlflow.log_param('cat__encoding', ce.strategy)
 
-                try:
-                    fs_params = fs.get_params()
-                    for k in fs_params.keys():
-                        mlflow.log_param('fs__'+k, fs_params[k])
-                except:
-                    pass
+            try:
+                fs_params = fs.get_params()
+                for k in fs_params.keys():
+                    mlflow.log_param('fs__'+k, fs_params[k])
+            except:
+                pass
 
-                estimator_parms = dict(list(est.get_params().items())
-                         + list(est.get_estimator().get_params().items()))
-                for k in estimator_parms.keys():
-                    mlflow.log_param('est__'+k, estimator_parms[k])
+            estimator_parms = dict(list(est.get_params().items())
+                     + list(est.get_estimator().get_params().items()))
+            for k in estimator_parms.keys():
+                mlflow.log_param('est__'+k, estimator_parms[k])
 
 
             try:
@@ -485,9 +484,8 @@ class Optimiser():
             print("CPU time: %s seconds" % (time.time() - start_time))
             print("")
 
-        if self.mlflow_active:
-            mlflow.log_metric(str(self.scoring), score)
-            mlflow.end_run()
+        mlflow.log_metric(str(self.scoring), score)
+        mlflow.end_run()
 
         return score
 
